e5c14cfca10e2b30d610ed86c75a2d854439d7eb
Nemsh <Artnemshilov@gmail.com>
2024-06-26 00:02:08 +0200

    finish ?

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from toolkit import tokenize
+from toolkit import tokenize_text, pos_tag_text, chunk_text
 
 templates = Jinja2Templates(directory="templates")
 app = FastAPI()
@@ -14,11 +14,33 @@ def read_root(request: Request):
 
 
 @app.get("/tokenizer", response_class=HTMLResponse)
-def read_page1(request: Request):
+def read_tokenizer(request: Request):
     return templates.TemplateResponse("tokenizer.html", {"request": request})
+
+
+@app.get("/pos_tagger", response_class=HTMLResponse)
+def read_pos_tag(request: Request):
+    return templates.TemplateResponse("pos_tagger.html", {"request": request})
+
+
+@app.get("/chunker", response_class=HTMLResponse)
+def read_chunker(request: Request):
+    return templates.TemplateResponse("chunker.html", {"request": request})
+
+
+@app.post("/chunk", response_class=HTMLResponse)
+def chunk_endpoint(request: Request, text: str = Form(...)):
+    entities = chunk_text(text)
+    return templates.TemplateResponse("chunker.html", {"request": request, "entities": entities})
+
+
+@app.post("/pos_tag", response_class=HTMLResponse)
+def pos_tag_endpoint(request: Request, text: str = Form(...)):
+    pos_tags = pos_tag_text(text)
+    return templates.TemplateResponse("pos_tagger.html", {"request": request, 'pos_tags': pos_tags})
 
 
 @app.post("/tokenize", response_class=HTMLResponse)
 def tokenize_endpoint(request: Request, text: str = Form(...)):
-    tokens = tokenize(text)
+    tokens = tokenize_text(text)
     return templates.TemplateResponse("tokenizer.html", {"request": request, "tokens": tokens})
--- toolkit.py
+++ toolkit.py
@@ -2,9 +2,20 @@ import nltk
 from nltk.tokenize import word_tokenize
 
 
-def tokenize(text):
+def tokenize_text(text):
     return word_tokenize(text)
+
+
+def pos_tag_text(text):
+    return nltk.pos_tag(tokenize_text(text))
+
+
+def chunk_text(text):
+    return nltk.ne_chunk(pos_tag_text(text))
 
 
 if __name__ == "__main__":
     nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('maxent_ne_chunker')
+    nltk.download('words')
